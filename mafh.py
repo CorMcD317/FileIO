63706d5382691c03f8e1369b7ad57510bfe0691b
CorMcD317 <mcdoucor000@warren.k12.in.us>
2025-04-11 09:30:17 -0400

    Mafh Commit

--- mafh.py
+++ mafh.py
@@ -1,1 +1,14 @@
-def add
+def add(x, y):
+    return x + y
+
+
+def subtract(x, y):
+    return x - y
+
+
+def multiply(x, y):
+    return x * y
+
+
+def division(x, y):
+    return x / y
---
+++ main.py
@@ -0,0 +1,14 @@
+from mafh import add, subtract, multiply, division
+
+plus = add(input("x = "), input("y = "))
+
+diff = subtract(int(input("x =")), int(input("y =")))
+
+prod = multiply(int(input("x =")), int(input("y =")))
+
+quo = division(int(input("x =")), int(input("y =")))
+
+print(plus)
+print(diff)
+print(prod)
+print(quo)
